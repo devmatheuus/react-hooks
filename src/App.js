4f46548b563aadd3931799595ce6520e44fdddc8
Matheus <matheus_lsilva@hotmail.com>
2023-02-16 20:49:10 -0300

    useState hook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
+import './App.css';
+
+import { useState } from 'react';
+
+import logo from './logo.svg';
+
 export const App = () => {
+  const [reverse, setReverse] = useState(false);
+  const [counter, setCounter] = useState(0);
+
+  const reverseClass = reverse ? 'reverse' : '';
+
+  const handleClick = () => {
+    setReverse((reverse) => !reverse);
+  };
+
+  const handleIncrement = () => {
+    setCounter((counter) => counter + 1);
+  };
+
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <header className="App-header">
+        <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
+
+        <h1>Contador: {counter}</h1>
+
+        <p>
+          <button type="button" onClick={handleClick}>
+            Reverse {reverseClass}
+          </button>
+        </p>
+        <p>
+          <button type="button" onClick={handleIncrement}>
+            Increment {counter}
+          </button>
+        </p>
+      </header>
     </div>
   );
 };
