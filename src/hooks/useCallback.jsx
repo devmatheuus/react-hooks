d42fc5338180591c58da3eeed660bd0f0d68522c
Matheus <matheus_lsilva@hotmail.com>
2023-02-17 00:33:56 -0300

    useCallback and React.memo examples

---
+++ src/hooks/useCallback.jsx
@@ -0,0 +1,31 @@
+import P from 'prop-types';
+import React, { useCallback, useState } from 'react';
+
+const Button = React.memo(function Button({ incrementCounter }) {
+  console.log('Button rendeizado');
+
+  return <button onClick={() => incrementCounter(10)}>+</button>;
+});
+
+export const ExampleUseCallback = () => {
+  const [counter, setCounter] = useState(0);
+
+  console.log('UseCallback renderizado');
+
+  const incrementCounter = useCallback((number) => {
+    setCounter((prevState) => prevState + number);
+  }, []);
+
+  return (
+    <div className="App">
+      <h1 id="title">Teste</h1>
+
+      <h2>Contador: {counter}</h2>
+      <Button incrementCounter={incrementCounter} />
+    </div>
+  );
+};
+
+Button.propTypes = {
+  incrementCounter: P.func,
+};
